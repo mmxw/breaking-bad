764da7d23161e4b56e241efc128e1be8b1b8e7d8
David Campbell <dcampbell092@gmail.com>
2020-01-22 18:49:10 +0000

    cards formatted

--- src/components/characters/CharacterCard.js
+++ src/components/characters/CharacterCard.js
@@ -2,21 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const CharacterCard = ({ char_id, name, img, nickname }) => (
-  <div className="column is-one-quarter-desktop is-one-third-tablet if-full-mobile">
-
+  <div className="column is-one-quarter-desktop is-one-third-tablet is-full-mobile">
     <Link to={`/characters/${char_id}`}>
       <div className="card">
-        <h4 className="card-header-title">{name}/{nickname}</h4>
-
-        <figure className="image">
-          <img src={img} alt={name} />
-        </figure>
-
+        <div className="card-header">
+          <h4 className="card-header-title">{name}/{nickname}</h4>
+        </div>
+        <div className="card-image">
+          <figure className="image">
+            <img src={img} alt={name} />
+          </figure>
+        </div>
       </div>
     </Link>
   </div>
 )
 
 export default CharacterCard
-
-//TODO: Styling card, fix broken img link
