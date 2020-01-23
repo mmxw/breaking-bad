413e093c7ce683dbc8e00f8a3b037da2cf2ff7a5
David Campbell <dcampbell092@gmail.com>
2020-01-23 11:06:14 +0000

    fixed season links

--- src/components/seasons/SeasonIndex.js
+++ src/components/seasons/SeasonIndex.js
@@ -3,32 +3,40 @@ import SeasonCard from './SeasonCard'
 
 class SeasonIndex extends React.Component {
 
-  state = [
-    {
-      seasonId: '1',
-      img: 'https://images.amcnetworks.com/blogs.amctv.com/wp-content/uploads/2013/12/bb-s1-poster.jpg'
-    },
-    {
-      seasonId: '2',
-      img: 'https://images.amcnetworks.com/blogs.amctv.com/wp-content/uploads/2013/12/bb-s2-poster.jpg'
-    },
-    {
-      seasonId: '3',
-      img: 'https://images.amcnetworks.com/blogs.amctv.com/wp-content/uploads/2013/12/BB-s3-poster.jpg'
-    },
-    {
-      seasonId: '4',
-      img: 'https://images.amcnetworks.com/blogs.amctv.com/wp-content/uploads/2013/12/bb-s4-poster.jpg'
-    },
-    {
-      seasonId: '5',
-      img: 'https://images.amcnetworks.com/blogs.amctv.com/wp-content/uploads/2013/12/bb-s5-poster.jpg'
-    }
-  ]
+  state = {
+    seasons: [
+      {
+        seasonId: '1',
+        img: 'https://images.amcnetworks.com/blogs.amctv.com/wp-content/uploads/2013/12/bb-s1-poster.jpg'
+      },
+      {
+        seasonId: '2',
+        img: 'https://images.amcnetworks.com/blogs.amctv.com/wp-content/uploads/2013/12/bb-s2-poster.jpg'
+      },
+      {
+        seasonId: '3',
+        img: 'https://images.amcnetworks.com/blogs.amctv.com/wp-content/uploads/2013/12/BB-s3-poster.jpg'
+      },
+      {
+        seasonId: '4',
+        img: 'https://images.amcnetworks.com/blogs.amctv.com/wp-content/uploads/2013/12/bb-s4-poster.jpg'
+      },
+      {
+        seasonId: '5',
+        img: 'https://images.amcnetworks.com/blogs.amctv.com/wp-content/uploads/2013/12/bb-s5-poster.jpg'
+      }
+    ]
+  }
 
   render() {
     return (
-      <h1>text</h1>
+      <section className="section">
+        <div className="container">
+          <div className="columns is-mobile is-multiline">
+            {this.state.seasons.map(season => <SeasonCard key={season.seasonId} {...season} />)}
+          </div>
+        </div>
+      </section>
     )
   }
 }
