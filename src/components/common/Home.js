2cc6f6cc175ec4063d37202fc58a9fec084ee152
David Campbell <dcampbell092@gmail.com>
2020-01-24 13:09:09 +0000

    added youtube background

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -1,27 +1,16 @@
 import React from 'react'
-import Background from '../../assets/hero.jpg'
-
-const sectionStyle = {
-  width: '100%',
-  backgroundImage: `url(${Background})`,
-  backgroundRepeat: 'no-repeat',
-  backgroundAttachment: 'fixed',
-  backgroundSize: 'cover'
-}
+import YoutubeBackground from 'react-youtube-background'
 
 const Home = () => (
-  <section style={sectionStyle} className="hero is-fullheight-with-navbar">
-    <div className="hero-body cheese-image">
-      <div className="container">
-        <p className="title is-1 has-text-black has-text-centered">
-          
-        </p>
-        <p className="subtitle is-4 has-text-black has-text-centered">
-          
-        </p>
+
+  <YoutubeBackground videoId={'a02i_Ik4GGM'}>
+    <section className="hero is-fullheight-with-navbar">
+      <div className="hero-body cheese-image">
+        <div className="container">
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </YoutubeBackground >
 )
 
 export default Home
