4aa21c813c5bc95183b6634ed4372412887f5260
David Campbell <dcampbell092@gmail.com>
2020-02-04 20:51:13 +0000

    edit

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -5,7 +5,7 @@ const Home = () => (
 
   <YoutubeBackground videoId={'a02i_Ik4GGM'}>
     <section className="hero is-fullheight-with-navbar">
-      <div className="hero-body cheese-image">
+      <div className="hero-body">
         <div className="container">
         </div>
       </div>
